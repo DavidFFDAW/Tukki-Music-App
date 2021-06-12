9c373e9452758ba326a0bc04c7e190923a393cbb
DavidFFDAW <davidferflo2@gmail.com>
2021-06-13 01:45:23 +0200

    feat(root): added the design XD template and testing file

--- testes.js
+++
@@ -1,9 +0,0 @@
-const input = 'Aisharawa';
-
-const filterLetter = input.split('').filter(letter => {
-    let previousLetter = letter;
-
-    if(letter === previousLetter) {
-        returningValue = letter;
-    }
-})
